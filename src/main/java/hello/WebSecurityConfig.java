c7fe07b941c020b2796685d7331c026e9d053399
Timo Kankaanpää <tka@puv.fi>
2020-02-07 10:54:08 +0200

    Update WebSecurityConfig.java

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,23 +25,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		        .contextSource()
 		        .url("ldap://ldap.puv.fi:389/dc=puv,dc=fi");
 	}
-	
-	/*old version using application.properties and spring.ldap.embedded.ldif=classpath:test-server.ldif
-	 	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.ldapAuthentication()
-				.userDnPatterns("uid={0},ou=people")
-				.groupSearchBase("ou=groups")
-				.contextSource(contextSource())
-				.passwordCompare()
-					.passwordEncoder(new LdapShaPasswordEncoder())
-					.passwordAttribute("userPassword");
-	}
-
-	@Bean
-	public DefaultSpringSecurityContextSource contextSource() {
-		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
-	}
-	 */
 }
